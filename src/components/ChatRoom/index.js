8b94d629507b5897268eff211c9eb928cca9515c
Marc Mariné <shenobi@gmail.com>
2021-06-14 19:29:03 +0200

    Improve message payload

--- src/components/ChatRoom/index.js
+++ src/components/ChatRoom/index.js
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import { connect } from 'react-redux'
+import { nanoid } from 'nanoid'
 import { addMessage } from '../../actions'
 import { Wrapper } from './styles'
 
 function ChatRoom({ userName, messages, addMessage }) {
   const [newMessage, setNewMessage] = useState(undefined)
-  
+
   const handleSubmit = event => {
     event.preventDefault()
-    addMessage(newMessage)
+    addMessage({
+      id: nanoid(12),
+      author: userName,
+      displayMessage: newMessage,
+      publishedAt: new Date()
+    })
     event.target['message'].value = ''
     setNewMessage(undefined)
   }
@@ -17,7 +23,7 @@ function ChatRoom({ userName, messages, addMessage }) {
     <Wrapper>
       <ul>
         <li className="message message--in">Buenas tardes {userName}. ¿Qué tal todo?</li>
-        {messages.map((message, index) => <li key={`message${index}`} className="message message--out">{message}</li>)}
+        {messages.map(({id, displayMessage}) => <li key={id} className="message message--out">{displayMessage}</li>)}
       </ul>
       <form onSubmit={handleSubmit}>
         <input onChange={event => setNewMessage(event.target.value)} id="message" type="text" autoFocus autoComplete="off" placeholder="Escribe un mensaje..." />
