7795d7f3fbd12569ed17118b2c4abcca1270b2dc
Marc Mariné <shenobi@gmail.com>
2021-06-15 10:25:19 +0200

    Add some extra styles

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -12,7 +12,11 @@ export const Wrapper = styled.div`
 
 export const Screen = styled.div`
   background-color: whitesmoke;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 48 48'%3E%3Cg fill='%23dddddd' fill-opacity='0.4'%3E%3Cpath d='M12 0h18v6h6v6h6v18h-6v6h-6v6H12v-6H6v-6H0V12h6V6h6V0zm12 6h-6v6h-6v6H6v6h6v6h6v6h6v-6h6v-6h6v-6h-6v-6h-6V6zm-6 12h6v6h-6v-6zm24 24h6v6h-6v-6z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 80 80' width='80' height='80'%3E%3Cpath fill='%23dddddd' fill-opacity='0.4' d='M14 16H9v-2h5V9.87a4 4 0 1 1 2 0V14h5v2h-5v15.95A10 10 0 0 0 23.66 27l-3.46-2 8.2-2.2-2.9 5a12 12 0 0 1-21 0l-2.89-5 8.2 2.2-3.47 2A10 10 0 0 0 14 31.95V16zm40 40h-5v-2h5v-4.13a4 4 0 1 1 2 0V54h5v2h-5v15.95A10 10 0 0 0 63.66 67l-3.47-2 8.2-2.2-2.88 5a12 12 0 0 1-21.02 0l-2.88-5 8.2 2.2-3.47 2A10 10 0 0 0 54 71.95V56zm-39 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm40-40a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM15 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm40 40a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'%3E%3C/path%3E%3C/svg%3E");
+  background-position-x: 4px;
   border-radius: ${BORDER_RADIUS};
+  box-sizing: border-box;
   height: 30em;
   margin-left: auto;
   margin-right: auto;
@@ -22,6 +26,11 @@ export const Screen = styled.div`
   position: relative;
   transition: all 500ms ease-in-out;
   width: 100%;
+
+  @media (max-width: 30em) {
+    border-radius: 0;
+    height: 100vh;
+  }
 
   ::before {
     background-color: ${COLOR_PRIMARY};
@@ -57,6 +66,7 @@ export const Button = styled.button`
   border: none;
   color: inherit;
   cursor: pointer;
+  display: flex;
   font-size: inherit;
   transition: opacity 500ms ease-in-out;
   ${({ disabled, variant }) => `
